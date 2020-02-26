ac24e7383ee05aa7e743fa3fc5bd55d942f39471
kelbikhanov <rusik9091@gmail.com>
2020-02-26 07:04:48 +0300

    Updates

---
+++ precache-manifest.0434b36eac75865739ef83e672a2b941.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a626fecc7eecc5678452a9a0f3d096d",
+    "url": "/Kelbikhanov/Social/index.html"
+  },
+  {
+    "revision": "f2482a04345dfaef9a0b",
+    "url": "/Kelbikhanov/Social/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "662e169651c5b0ea29f2",
+    "url": "/Kelbikhanov/Social/static/css/3.543e51a7.chunk.css"
+  },
+  {
+    "revision": "88f50d7c61d20b05e4e0",
+    "url": "/Kelbikhanov/Social/static/css/4.b7b4d88b.chunk.css"
+  },
+  {
+    "revision": "9d6d724ef2aab58bcdfe",
+    "url": "/Kelbikhanov/Social/static/css/main.80d17736.chunk.css"
+  },
+  {
+    "revision": "f2482a04345dfaef9a0b",
+    "url": "/Kelbikhanov/Social/static/js/2.e4e10ea3.chunk.js"
+  },
+  {
+    "revision": "662e169651c5b0ea29f2",
+    "url": "/Kelbikhanov/Social/static/js/3.23ec3597.chunk.js"
+  },
+  {
+    "revision": "88f50d7c61d20b05e4e0",
+    "url": "/Kelbikhanov/Social/static/js/4.f922b0c0.chunk.js"
+  },
+  {
+    "revision": "9d6d724ef2aab58bcdfe",
+    "url": "/Kelbikhanov/Social/static/js/main.d98eca97.chunk.js"
+  },
+  {
+    "revision": "5e3b0635c5544e43bf08",
+    "url": "/Kelbikhanov/Social/static/js/runtime-main.b17240c9.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/Kelbikhanov/Social/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/Kelbikhanov/Social/static/media/user.8b1dbb1f.png"
+  }
+]);
